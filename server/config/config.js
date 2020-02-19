0c12be4c9d9996ad6328564dff01ab00a7bb12d9
camiloadominguez <camiloadoming@hotmail.com>
2020-02-18 19:47:17 -0500

    Cambio del nombre de la base de datos

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlDB
 if(process.env.NODE_ENV==='dev'){
     urlDB='mongodb://localhost:27017/coffee'
 }else{
-    urlDB='mongodb+srv://Admin_coffee:D7Vehv7ahP3LUGH6@cluster0-g3jco.mongodb.net/test1'
+    urlDB='mongodb+srv://Admin_coffee:D7Vehv7ahP3LUGH6@cluster0-g3jco.mongodb.net/coffee'
 }
 
 process.env.URLDB=urlDB;
